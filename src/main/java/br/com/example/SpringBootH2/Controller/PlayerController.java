ab0870a1e248ea6160791a38754ed0db98d41f38
João Vitor Motta Silva <joao.motta@S2IT.CORP>
2018-05-18 20:50:45 -0300

    Swagger implementation

--- src/main/java/br/com/example/SpringBootH2/Controller/PlayerController.java
+++ src/main/java/br/com/example/SpringBootH2/Controller/PlayerController.java
@@ -2,6 +2,8 @@ package br.com.example.SpringBootH2.Controller;
 
 import br.com.example.SpringBootH2.Entity.Player;
 import br.com.example.SpringBootH2.Service.PlayerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,33 +13,41 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@Api(value = "RestFull API using SpringBoot, H2 and Swagger")
 public class PlayerController {
     
     @Autowired
     private PlayerService playerService;
 
     @GetMapping("/player")
+    @ApiOperation(value = "Return a list of players.")
     public List<Player> getAll(){
         return this.playerService.getAll();
     }
 
     @GetMapping("/player/{id}")
+    @ApiOperation(value = "Return a player according to id.")
     public Optional<Player> findById(@PathVariable Long id){
         return this.playerService.findById(id);
     }
 
     @PostMapping("/player")
+    @ApiOperation(value = "Create a new player in database.")
     public void insert(@RequestBody Player player){
         this.playerService.insert(player);
     }
 
     @DeleteMapping("/player/{id}")
-    public void delete(@PathVariable Long id){
+    @ApiOperation(value = "Delete a player from database")
+    public void delete(@PathVariable Long id)
+    {
         this.playerService.delete(id);
     }
 
     @PutMapping("/player/{id}")
-    public void update(@PathVariable Long id, @RequestBody Player player){
+    @ApiOperation(value = "Update data from a player according to id")
+    public void update(@PathVariable Long id, @RequestBody Player player)
+    {
         this.playerService.update(player,id);
     }
 }
--- src/main/java/br/com/example/SpringBootH2/SpringBootH2Application.java
+++ src/main/java/br/com/example/SpringBootH2/SpringBootH2Application.java
@@ -1,15 +1,56 @@
 package br.com.example.SpringBootH2;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
+@EnableSwagger2
 public class SpringBootH2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootH2Application.class, args);
+	}
+
+	@Bean
+	public Docket swaggerSettings() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.example.SpringBootH2"))
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.apiInfo(this.info());
+	}
+
+	private ApiInfo info(){
+		ApiInfo info = new ApiInfo(
+				"Player Management API",
+				"RestFull API using SpringBoot, H2 and Swagger2",
+				"1.0",
+				null,
+				new Contact("João Vitor Motta","https://github.com/joaovmotta","jaoo.motta@gmail.com"),
+				null,
+				null,
+				new ArrayList<VendorExtension>()
+		);
+		return info;
 	}
 }
