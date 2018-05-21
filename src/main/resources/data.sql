2873b691c2bbf8a0f1f470ffd5e30d74b1aed8b2
João Vitor Motta Silva <joao.motta@S2IT.CORP>
2018-05-21 08:38:08 -0300

    New data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO player (name,birthday,weight,height,sport) VALUES ('Magal','1991-10-10',60,1.7,'football');
+
+INSERT INTO player (name,birthday,weight,height,sport) VALUES ('Tadeu Tadado','1995-11-10',55,1.7,'basketball');
+
+INSERT INTO player (name,birthday,weight,height,sport) VALUES ('Raul Seixas','1996-09-10',80,1.9,'karate');
+
+INSERT INTO player (name,birthday,weight,height,sport) VALUES ('Kabelo','1992-02-10',60.9,2,'swimming');
+
+INSERT INTO player (name,birthday,weight,height,sport) VALUES ('Guilherme Zupolini','1988-01-10',160,1.6,'baseball');
+
+INSERT INTO player (name,birthday,weight,height,sport) VALUES ('Rafael costa','1997-06-10',90,1.7,'soccer')
