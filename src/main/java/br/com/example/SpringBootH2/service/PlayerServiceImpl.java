fdac9cc511e67d117527a1e3b534b726252f1c78
Vitor Motta <jaoo.motta@gmail.com>
2018-09-13 11:40:33 -0300

    Adicionado anotações transacionais.

--- src/main/java/br/com/example/SpringBootH2/service/PlayerServiceImpl.java
+++ src/main/java/br/com/example/SpringBootH2/service/PlayerServiceImpl.java
@@ -1,13 +1,14 @@
 package br.com.example.SpringBootH2.service;
 
-import br.com.example.SpringBootH2.representation.entity.Player;
 import br.com.example.SpringBootH2.infra.exception.ResourceNotFoundException;
 import br.com.example.SpringBootH2.mapper.PlayerMapper;
 import br.com.example.SpringBootH2.repository.PlayerRepository;
+import br.com.example.SpringBootH2.representation.entity.Player;
 import br.com.example.SpringBootH2.representation.request.PlayerRequest;
 import br.com.example.SpringBootH2.representation.response.PlayerResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,22 +21,27 @@ public class PlayerServiceImpl implements PlayerService {
     @Autowired
     private PlayerMapper playerMapper;
 
+    @Transactional(readOnly = true)
     public List<PlayerResponse> findAll() {
         return this.playerMapper.playerToResponse((List<Player>) this.playerRepository.findAll());
     }
 
+    @Transactional
     public Long insert(PlayerRequest player) {
         return this.playerRepository.save(this.playerMapper.requestToPlayer(player)).getId();
     }
 
+    @Transactional(readOnly = true)
     public Player findById(Long id) {
         return this.playerRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
     }
 
+    @Transactional
     public void delete(Long id) {
         this.playerRepository.deleteById(id);
     }
 
+    @Transactional
     public void update(PlayerRequest player, Long id) {
         if (this.playerRepository.existsById(id)) {
             Player playerEntity = this.playerMapper.requestToPlayer(player);
